7019ef0e963e1b2ed75809059e2121d4290fa1ac
oscarlos Gomez Rosario <oscarlosg14@hotmail.com>
2023-10-17 19:05:20 -0400

    created some functionality. fixed a bug with the handlenav function

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -1,18 +1,22 @@
 import React, { useState } from "react";
 import { AiOutlineMenu } from "react-icons/ai";
 
-const sidebar = () => {
-  const [nav, SetNav] = useState(false);
+const SideNav = () => {
+  const [nav, setNav] = useState(false);
 
-  handleNav = () => {
-     SetNav(!nav)
+  const handleNav = () => {
+    setNav(!nav);
   };
+  
 
   return (
     <div>
-      <AiOutlineMenu />
+      <AiOutlineMenu
+        onClick={handleNav}
+        className="absolute top-4 right-4 z-[99] md:hidden"
+      />
     </div>
   );
 };
 
-export default sidebar;
+export default SideNav;
