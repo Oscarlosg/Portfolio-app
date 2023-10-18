b571a249c9c0bc90b4d9030926f4a8158634e060
oscarlos Gomez Rosario <oscarlosg14@hotmail.com>
2023-10-17 21:52:52 -0400

    created sidebar for when the screen is above median width. Feeling great

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -26,14 +26,14 @@ const SideNav = () => {
             <span className="pl-4">Home</span>
           </a>
           <a
-            href="#main"
+            href="#work"
             className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shado-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
           >
             <GrProjects size={20} />
             <span className="pl-4">Work</span>
           </a>
           <a
-            href="#main"
+            href="#projects"
             className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shado-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
           >
             <AiOutlineProject size={20} />
@@ -47,7 +47,7 @@ const SideNav = () => {
             <span className="pl-4">Resume</span>
           </a>
           <a
-            href="#main"
+            href="#contact"
             className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shado-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
           >
             <AiOutlineMail size={20} />
@@ -55,8 +55,27 @@ const SideNav = () => {
           </a>
         </div>
       ) : (
-        <></>
+        null
       )}
+      <div className="md:block hidden fixed top-[25%] z-10">
+        <div className="flex flex-col">
+          <a href="#main" className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 ml-5 cursor-pointer hover:scale-110 ease-in duration-300">
+            <AiOutlineHome size={20}/>
+          </a>
+          <a href="#work" className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 ml-5 cursor-pointer hover:scale-110 ease-in duration-300">
+            <GrProjects size={20}/>
+          </a>
+          <a href="#projects" className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 ml-5 cursor-pointer hover:scale-110 ease-in duration-300">
+            <AiOutlineProject size={20}/>
+          </a>
+          <a href="#main" className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 ml-5 cursor-pointer hover:scale-110 ease-in duration-300">
+            <BsPerson size={20}/>
+          </a>
+          <a href="#contact" className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 ml-5 cursor-pointer hover:scale-110 ease-in duration-300">
+            <AiOutlineMail size={20}/>
+          </a>
+        </div>
+      </div>
     </div>
   );
 };
