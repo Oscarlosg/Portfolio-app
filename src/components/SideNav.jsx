e615e440c23a88bcb523a480255b62f43b2d1bcb
oscarlos Gomez Rosario <oscarlosg14@hotmail.com>
2023-10-17 21:12:33 -0400

    finished creating sidenav outline and initial styling.

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
-import { AiOutlineMenu } from "react-icons/ai";
+import { AiOutlineMenu, AiOutlineHome, AiOutlineProject, AiOutlineMail } from "react-icons/ai";
+import { Bsperson } from "react-icons/bs";
+import { GrProjects } from "react-icons/gr";
 
 const SideNav = () => {
   const [nav, setNav] = useState(false);
@@ -7,7 +9,6 @@ const SideNav = () => {
   const handleNav = () => {
     setNav(!nav);
   };
-  
 
   return (
     <div>
@@ -15,6 +16,47 @@ const SideNav = () => {
         onClick={handleNav}
         className="absolute top-4 right-4 z-[99] md:hidden"
       />
+      {nav ? (
+        <div className="fixed w-full h-screen bg-white/90 flex flex-col justify-center items-center z-20">
+          <a
+            href="#main"
+            className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shado-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
+          >
+            <AiOutlineHome size={20} />
+            <span className="pl-4">Home</span>
+          </a>
+          <a
+            href="#main"
+            className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shado-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
+          >
+            <GrProjects size={20} />
+            <span className="pl-4">Work</span>
+          </a>
+          <a
+            href="#main"
+            className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shado-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
+          >
+            <AiOutlineHome size={20} />
+            <span className="pl-4">Projects</span>
+          </a>
+          <a
+            href="#main"
+            className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shado-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
+          >
+            <AiOutlineHome size={20} />
+            <span className="pl-4">Resume</span>
+          </a>
+          <a
+            href="#main"
+            className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shado-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
+          >
+            <AiOutlineHome size={20} />
+            <span className="pl-4">Contact</span>
+          </a>
+        </div>
+      ) : (
+        <></>
+      )}
     </div>
   );
 };
