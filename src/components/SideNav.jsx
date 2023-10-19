04fc4134618f53cd7cfec130c753a5132af616af
oscarlos Gomez Rosario <oscarlosg14@hotmail.com>
2023-10-19 19:03:01 -0400

    added extra functionality to the side nav

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -8,8 +8,6 @@ import {
 import { BsPerson } from "react-icons/bs";
 import { GrProjects } from "react-icons/gr";
 
-
-
 const SideNav = () => {
   const [nav, setNav] = useState(false);
 
@@ -26,35 +24,40 @@ const SideNav = () => {
       />
       {nav ? (
         <div className="fixed w-full h-screen bg-white/90 flex flex-col justify-center items-center z-20">
-          <a onClick={handleNav}
+          <a
+            onClick={handleNav}
             href="#main"
             className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shado-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
           >
             <AiOutlineHome size={20} />
             <span className="pl-4">Home</span>
           </a>
-          <a onClick={handleNav}
+          <a
+            onClick={handleNav}
             href="#work"
             className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shado-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
           >
             <GrProjects size={20} />
             <span className="pl-4">Work</span>
           </a>
-          <a onClick={handleNav}
+          <a
+            onClick={handleNav}
             href="#projects"
             className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shado-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
           >
             <AiOutlineProject size={20} />
             <span className="pl-4">Projects</span>
           </a>
-          <a onClick={handleNav}
+          <a
+            onClick={handleNav}
             href="#main"
             className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shado-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
           >
             <BsPerson size={20} />
             <span className="pl-4">Resume</span>
           </a>
-          <a onClick={handleNav}
+          <a
+            onClick={handleNav}
             href="#contact"
             className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shado-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
           >
@@ -65,12 +68,63 @@ const SideNav = () => {
       ) : null}
       <div className="md:block hidden fixed top-[25%] z-10">
         <div className="flex flex-col">
-          <a
-            href="#main"
-            className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 ml-5 cursor-pointer hover:scale-110 ease-in duration-300"
-          >
-            <AiOutlineHome size={20} />
-          </a>
+          <div className="flex flex-row group">
+            <a
+              href="#main"
+              className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 ml-5 cursor-pointer hover:scale-110 ease-in duration-300"
+            >
+              <AiOutlineHome size={20} />
+            </a>
+            <p className="pt-[22px] text-black-400 hidden group-hover:block drop-shadow-xl shadow-white">
+              Home
+            </p>
+          </div>
+          <div className="flex flex-row group">
+            <a
+              href="#work"
+              className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 ml-5 cursor-pointer hover:scale-110 ease-in duration-300"
+            >
+              <GrProjects size={20} />
+            </a>
+            <p className="pt-[22px] text-black-400 hidden group-hover:block drop-shadow-xl shadow-white">
+              Work History
+            </p>
+          </div>
+          <div className="flex flex-row group">
+            <a
+              href="#projects"
+              className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 ml-5 cursor-pointer hover:scale-110 ease-in duration-300"
+            >
+              <AiOutlineProject size={20} />
+            </a>
+            <p className="pt-[22px] text-black-400 hidden group-hover:block drop-shadow-xl shadow-white">
+              Projects
+            </p>
+          </div>
+          <div className="flex flex-row group">
+            <a
+              href="#main"
+              className="rounded-full shadow-lg bg-gray-100 shadow-gray-200 m-2 p-4 ml-5 cursor-pointer hover:scale-110 ease-in duration-300"
+            >
+              <BsPerson size={20} />
+            </a>
+            <p className="pt-[22px] text-black-400 hidden group-hover:block drop-shadow-xl shadow-white">
+              Resume
+            </p>
+          </div>
+          <div className="flex flex-row group">
+            <a
+              href="#contact"
+              className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 ml-5 cursor-pointer hover:scale-110 ease-in duration-300"
+            >
+              <AiOutlineMail size={20} />
+            </a>
+            <p className="pt-[22px] text-black-400 hidden group-hover:block drop-shadow-xl shadow-white">
+              Contact
+            </p>
+          </div>
+          {/*
+          old version of buttons, may delete later
           <a
             href="#work"
             className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 ml-5 cursor-pointer hover:scale-110 ease-in duration-300"
@@ -94,7 +148,7 @@ const SideNav = () => {
             className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 ml-5 cursor-pointer hover:scale-110 ease-in duration-300"
           >
             <AiOutlineMail size={20} />
-          </a>
+          </a> */}
         </div>
       </div>
     </div>
