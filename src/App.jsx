63cd479d2d831346eafe7efce6aad2b3a08bfd30
oscarlos Gomez Rosario <oscarlosg14@hotmail.com>
2023-10-19 14:32:09 -0400

    finished contact form

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import SideNav from './components/SideNav'
 import Main from './components/main'
 import Work from './components/Work'
 import Projects from './components/Projects'
+import Contact from './components/Contact'
 
 function App() {
 
@@ -15,6 +16,7 @@ function App() {
      <Main />
      <Work />
      <Projects />
+     <Contact />
     </>
   )
 }
