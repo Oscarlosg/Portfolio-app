37786f4dfd94a55f7affd9830c6615a9562e9b07
oscarlos Gomez Rosario <oscarlosg14@hotmail.com>
2023-10-25 21:56:07 -0400

    added real data to project section, tweaked some of the project components so they are more dynamic

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const ProjectItem = ({ img, title }) => {
+const ProjectItem = ({ img, title, technology, url }) => {
   return (
     <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl group hover:bg-gradient-to-r from-gray-400 to-[#0C4160]">
       <img
@@ -13,9 +13,9 @@ const ProjectItem = ({ img, title }) => {
           {title}
         </h3>
         <p className="pb-4 pt-2 text-white text-center">
-          technology{/* add prop for technology used in project */}
+          {technology}{/* add prop for technology used in project */}
         </p>
-        <a href="/" className="">
+        <a href={url} className="">
             <p className="text-center p-3 rounded-lg bg-white text-gray-700 font-bold cursor-pointer text-lg ">More Info</p>
         </a>
       </div>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ProjectItem from "./ProjectItem";
-import projectImageTemplate from "../assets/project-template.jpg";
+import projectList from "../assets/projectList.json";
 
 const Projects = () => {
   return (
@@ -9,17 +9,18 @@ const Projects = () => {
     <div id="projects" className="max-w-[1040px] m-auto md:pl-20 p-4 py-16">
       <h1 className="text-4xl font-bold text-center text-gray-400">Projects</h1>
       <p className="text-center py-8">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur.
+        Most of these were created while at school. Although some of them may be around the edges, I had a ton of fun learning and working on them regardless. I am working on updating some of these older projects and creating new ones so keep an eye out! 
       </p>
       <div className="grid sm:grid-cols-2 gap-12">
-        <ProjectItem img={projectImageTemplate} title="template" />
-        <ProjectItem img={projectImageTemplate} title="template" />
-        <ProjectItem img={projectImageTemplate} title="template" />
-        <ProjectItem img={projectImageTemplate} title="template" />
+      {projectList.map((item, idx) => (
+        <ProjectItem
+          key={idx}
+          img={item.img}
+          title={item.title}
+          technology={item.technology}
+          url={item.url}
+        />
+      ))}
       </div>
     </div>
   );
